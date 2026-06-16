2edc25eb3b286b6812674aefeca263b873bf9237
benxdon <brandonlin2006@gmail.com>
2026-06-16 17:22:53 +0800

    fixed the annoying errors with markdown files, reduce noise

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters = opts.linters or {}
+      opts.linters["markdownlint-cli2"] = {
+        args = {
+          "--config",
+          vim.fn.stdpath("config") .. "/.markdownlint.json",
+          "--",
+        },
+      }
+    end,
+  },
+}
